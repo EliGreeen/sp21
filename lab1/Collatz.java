c8a39661bbb552df0ac63b1ce07bea6344e9f19f
Green <1991317437@qq.com>
2024-08-31 18:27:44 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,7 +3,7 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** Buggy i*/
     public static int nextNumber(int n) {
         if (n  == 128) {
             return 1;
